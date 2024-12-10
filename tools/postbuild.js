ea7c0988d96cc6087dc9af79519c697b2b4c9f4c
Ravinder <ravinder@walkover.in>
2024-12-10 18:02:12 +0530

    Updated post build

--- tools/postbuild.js
+++ tools/postbuild.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 
 (async function build() {
-    await fs.copy('./demo.html', './dist/demo.html');
-    await fs.copy('./dist/chat-widget/main.js', './dist/chat-widget.js');
+    await fs.copy('./demo.html', './dist/chat-widget/demo.html');
+    await fs.copy('./dist/chat-widget/main.js', './dist/chat-widget/chat-widget.js');
     console.info('Elements created successfully!');
 })();
