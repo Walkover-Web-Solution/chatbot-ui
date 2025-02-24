b774aac5f9d2c2f50c8b591e8a73f76542912aae
Ravinder <ravinder@walkover.in>
2025-02-24 16:32:24 +0530

    Added stage env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,6 @@ import { envVariables } from './env-variables';
 export const environment = {
     production: true,
     server: 'https://control.msg91.com',
-    adminServer: 'https://vijay.msg91.com',
     env: 'prod',
     basePath: '',
     ...envVariables,
