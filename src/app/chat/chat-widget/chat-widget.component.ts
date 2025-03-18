2a8c72157fc442840870f826b50164bd56a0abbb
hemant gangwal <hemantkumargangwal@gmail.com>
2025-03-18 11:48:46 +0530

    fix uuid issue when sign up

--- src/app/chat/chat-widget/chat-widget.component.ts
+++ src/app/chat/chat-widget/chat-widget.component.ts
@@ -572,6 +572,7 @@ export class ChatWidgetComponent extends BaseComponent implements OnInit, OnDest
                 // Issue fixed related to this card
                 // https://app.clickup.com/t/86cu9eu1x
                 removeCookie('hello-widget-anonymous-uuid');
+                removeCookie('hello-widget-uuid')
             } else {
                 this.client_uuid =
                     this.getCookieValue('hello-widget-uuid') || this.getCookieValue('hello-widget-anonymous-uuid');
