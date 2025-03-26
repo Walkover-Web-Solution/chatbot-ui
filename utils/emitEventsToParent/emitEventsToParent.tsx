f6163eda4aaa0e3cd819aac9d487397a8caad740
Rudraksh Kachhawa <rudra007@Rudrakshs-Mac-mini.local>
2025-03-26 15:35:51 +0530

    feature -> send event to parent on bridge switch

--- utils/emitEventsToParent/emitEventsToParent.tsx
+++ utils/emitEventsToParent/emitEventsToParent.tsx
@@ -1,4 +1,4 @@
-export function emitEventToParent(type: 'FRONT_END_ACTION' | 'HEADER_BUTTON_PRESS' | 'MESSAGE_CLICK', data: any) {
+export function emitEventToParent(type: 'FRONT_END_ACTION' | 'HEADER_BUTTON_PRESS' | 'MESSAGE_CLICK' | 'BRIDGE_SWITCH', data: any) {
   const eventData = {
     type,
     data,
