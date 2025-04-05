bd3591612d3f943719252f8a206e2e6e74a11f22
parakhjain23 <parakhjain2301@gmail.com>
2025-04-05 16:59:10 +0530

    ..

--- store/hello/helloReducer.ts
+++ store/hello/helloReducer.ts
@@ -1,5 +1,6 @@
 import { SliceCaseReducers, ValidateSliceCaseReducers } from "@reduxjs/toolkit";
 import { $HelloReduxType } from "../../types/hello/HelloReduxType.ts";
+import actionType from "@/types/utility.js";
 
 export const initialState: $HelloReduxType = {
   isHuman: false,
@@ -25,7 +26,7 @@ export const reducers: ValidateSliceCaseReducers<
     state.anonymousClientId = anonymousClientId;
     state.socketJwt = { jwt: Jwt };
     state.ChannelList = ChannelList;
-    state.isHuman = ChannelList?.channels?.[0]?.channel || false;
+    // state.isHuman = ChannelList?.channels?.[0]?.channel || false;
     state.isLoading = false;
     state.Channel = ChannelList?.channels?.[0];
     state.mode = mode;
@@ -35,7 +36,7 @@ export const reducers: ValidateSliceCaseReducers<
     state.Channel = action.payload.Channel;
     state.isHuman = true;
   },
-  setHuman(state, action) {
+  setHuman(state, action: actionType<{ isHuman: boolean }>) {
     state.isHuman = action.payload?.isHuman ?? true;
   },
 };
