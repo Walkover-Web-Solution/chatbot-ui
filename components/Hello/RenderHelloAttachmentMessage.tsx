9de9a6271033eb879ff3bbca6f152449ad22042a
parakhjain23 <parakhjain2301@gmail.com>
2025-05-07 19:12:25 +0530

    add linkify for html render

--- components/Hello/RenderHelloAttachmentMessage.tsx
+++ components/Hello/RenderHelloAttachmentMessage.tsx
@@ -1,25 +1,22 @@
 import { useMediaQuery } from "@mui/material";
 import ImageWithFallback from "../Interface-Chatbot/Messages/ImageWithFallback";
+import { linkify } from "@/utils/utilities";
 
 function RenderHelloAttachmentMessage({ message }: { message: any }) {
 
   const caption = message?.messageJson?.text;
 
   const renderAttachment = (attachment: any) => {
-    const {path, name } = attachment;
-    return     <div className="w-full my-2">
-    <div className="flex gap-2">
-  
-<ImageWithFallback
- src={path}
- alt={name || 'Image attachment'}
- style={{ maxHeight: '300px' }}
-  />
-
-
+    const { path, name } = attachment;
+    return <div className="w-full my-2">
+      <div className="flex gap-2">
+        <ImageWithFallback
+          src={path}
+          alt={name || 'Image attachment'}
+          style={{ maxHeight: '300px' }}
+        />
+      </div>
     </div>
-
-  </div>
   };
 
   return (
@@ -29,13 +26,13 @@ function RenderHelloAttachmentMessage({ message }: { message: any }) {
           {renderAttachment(item)}
         </div>
       ))}
-          {caption && (
-      <div className="flex justify-between items-center w-full mt-1">
-        <div className="prose max-w-none">
-                <div dangerouslySetInnerHTML={{ __html: message?.content }}></div>
-              </div>
-      </div>
-    )}
+      {caption && (
+        <div className="flex justify-between items-center w-full mt-1">
+          <div className="prose max-w-none">
+            <div dangerouslySetInnerHTML={{ __html: linkify(message?.content) }}></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
