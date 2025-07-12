9773ee679366bb9f2b411b9d39955ad7d7c2e38d
parakhjain23 <parakhjain2301@gmail.com>
2025-07-12 18:35:36 +0530

    test - seperate useReducEffect from useReduxManagement for optimize redux dispatch

--- components/Interface-Chatbot/InterfaceChatbot.tsx
+++ components/Interface-Chatbot/InterfaceChatbot.tsx
@@ -1,72 +1,10 @@
 /* eslint-disable */
-import { HeaderButtonType } from "@/types/interface/InterfaceReduxType";
 import { createContext } from "react";
 
-interface MessageType {
-  content: string;
-  role: string;
-  responseId?: string;
-  wait?: boolean;
-  timeOut?: boolean;
-  createdAt?: string;
-  function?: () => void;
-  id?: string;
-  images?: string[]; // Added images property to MessageType
-  time?: string | number;
-}
 export const MessageContext = createContext<{
-  fetchChannels: () => void,
-  messages: MessageType[] | [];
-  messageIds: string[],
-  msgIdAndDataMap: { [msgId: string]: MessageType },
-  helloMsgIds: string[],
-  helloMsgIdAndDataMap: { [msgId: string]: any },
-  helloMessages: any;
-  addMessage?: (message: string) => void;
-  setMessages?: (message: MessageType[]) => void;
-  sendMessageToHello?: (() => void) | undefined
-  bridgeName?: string;
-  fetchMoreData?: () => void;
-  hasMoreMessages?: boolean;
-  setNewMessage?: (newMessage: boolean) => void;
-  newMessage?: boolean;
-  currentPage?: Number;
-  starterQuestions: string[];
-  headerButtons?: HeaderButtonType;
-  setOptions: (data: string[]) => void;
-  sendMessage: ({ message }: { message?: string }) => void;
-  loading?: boolean;
-  messageRef?: any;
-  disabled?: boolean;
-  options?: any[];
-  setChatsLoading?: any;
-  images: string[] | { path: string }[];
-  setImages: (data: string[] | { path: string }) => void;
-  setToggleDrawer: (data: boolean) => void;
-  setLoading: (data: boolean) => void,
-  isToggledrawer: boolean,
-  getMoreChats: () => void;
-  getMoreHelloChats: () => void;
-  handleMessageFeedback: (data: any) => void;
-  isTyping: boolean;
-
+  messageRef: React.RefObject<HTMLInputElement | HTMLTextAreaElement | null>;
+  timeoutIdRef: React.RefObject<ReturnType<typeof setTimeout> | null>;
 }>({
-  starterQuestions: [],
-  messages: [],
-  messageIds: [],
-  msgIdAndDataMap: {},
-  helloMessages: [],
-  headerButtons: [],
-  sendMessage: () => { },
-  images: [],
-  setImages: () => { },
-  setToggleDrawer: () => { },
-  setLoading: () => { },
-  isToggledrawer: false,
-  setOptions: () => { },
-  getMoreChats: () => { },
-  handleMessageFeedback: () => { },
-  helloMsgIds: [],
-  helloMsgIdAndDataMap: {},
-  isTyping: false
+  messageRef: { current: null },
+  timeoutIdRef: { current: null }
 });
