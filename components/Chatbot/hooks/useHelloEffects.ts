34a1f75b58b915be183502d4f58b20ade81ed4ac
parakhjain23 <parakhjain2301@gmail.com>
2025-07-30 19:14:09 +0530

    remove urlsToOpenInIFrame key from segmento data updation

--- components/Chatbot/hooks/useHelloEffects.ts
+++ components/Chatbot/hooks/useHelloEffects.ts
@@ -234,7 +234,7 @@ export const useHelloEffects = ({ chatSessionId, messageRef, tabSessionId }: Use
                 }
 
                 const keysToRemove = [
-                    'widgetToken', 'unique_id', 'user_jwt_token', 'sdkConfig', 'hide_launcher', 'show_widget_form', 'show_close_button', 'launch_widget', 'show_send_button', 'unique_id', 'primary_color', 'bot_id', 'name', 'number', 'mail', 'bot_type', 'isMobileSDK', 'pushConfig', 'variables'
+                    'widgetToken', 'unique_id', 'user_jwt_token', 'sdkConfig', 'hide_launcher', 'show_widget_form', 'show_close_button', 'launch_widget', 'show_send_button', 'unique_id', 'primary_color', 'bot_id', 'name', 'number', 'mail', 'bot_type', 'isMobileSDK', 'pushConfig', 'variables', 'urlsToOpenInIFrame'
                 ]
 
                 keysToRemove.map(key => {
