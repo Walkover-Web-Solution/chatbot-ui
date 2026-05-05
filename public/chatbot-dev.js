f2b4e80dc4f96b02404651e52f5239d3e04cbff7
Yogesh Patel <yogeshpatel@gmail.com>
2026-05-05 13:23:13 +0530

    fix: mode added in dev also

--- public/chatbot-dev.js
+++ public/chatbot-dev.js
@@ -76,7 +76,7 @@
                 'onOpen', 'onClose', 'iconColor', 'className', 'style', 'environment',
                 'fullScreen', 'hideCloseButton', 'hideIcon', 'parentId', 'config',
                 'headerButtons', 'eventsToSubscribe', 'modalConfig', 'allowModalSwitch',
-                'chatTitle', 'chatIcon', 'hideFullScreenButton', 'defaultOpen', 'theme'
+                'chatTitle', 'chatIcon', 'hideFullScreenButton', 'defaultOpen', 'theme', 'mode'
             ];
 
             return attributes.reduce((props, attr) => {
